748f46dd6aa45ef5fd53d886bdd32e7bc4f72143
yj <dfyd15937@gmail.com>
2025-04-10 11:22:52 +0900

    회원가입 기능 테스트

---
+++ src/main/java/com/esc/wmg/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.esc.wmg.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.esc.wmg.entity.UserEntity;
+import com.esc.wmg.repository.UserRepository;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    UserRepository repository;
+    
+    @PostMapping("/userInsert")
+    public String userInsert(UserEntity entity) {
+        System.out.println(entity.toString());
+        repository.save(entity);
+        return "redirect:/";
+    }
+
+
+    @GetMapping("/")
+    public String loginForm() {
+        return "login";
+    }
+}
