0b8de7fca8bc957eabed6be6859ca1cf1d89d31b
yj <dfyd15937@gmail.com>
2025-04-10 11:30:25 +0900

    회원가입 테스트

--- src/main/java/com/esc/wmg/model/Membermodel.java
+++
@@ -1,8 +0,0 @@
-package com.esc.wmg.model;
-
-import lombok.Data;
-
-@Data
-public class Membermodel {
-    
-}
--- src/main/java/com/esc/wmg/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.esc.wmg.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class MemberRepository {
-
-}
