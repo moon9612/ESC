8a9f7b470039ac2f99d5e5a4d5f30458e83acbbe
yj <dfyd15937@gmail.com>
2025-04-10 11:52:54 +0900

    회원정보 수정중

--- src/main/java/com/esc/wmg/controller/UserController.java
+++ src/main/java/com/esc/wmg/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.esc.wmg.entity.UserEntity;
 import com.esc.wmg.repository.UserRepository;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 public class UserController {
@@ -14,13 +16,21 @@ public class UserController {
     @Autowired
     UserRepository repository;
     
+
+    // 회원정보 수정기능
+    @PostMapping("/UserUpdate")
+    public String UserUpdate() {
+
+    }
+    
+
+    // 회원가입 기능
     @PostMapping("/userInsert")
     public String userInsert(UserEntity entity) {
         System.out.println(entity.toString());
         repository.save(entity);
         return "redirect:/";
     }
-
 
     @GetMapping("/")
     public String loginForm() {
