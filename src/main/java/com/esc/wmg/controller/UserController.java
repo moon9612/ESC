b701a0d5dc76a9a3cb1401e93e11b06bfac74aef
yj <dfyd15937@gmail.com>
2025-04-10 11:58:43 +0900

    af

--- src/main/java/com/esc/wmg/controller/UserController.java
+++ src/main/java/com/esc/wmg/controller/UserController.java
@@ -8,11 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.esc.wmg.entity.UserEntity;
 import com.esc.wmg.model.User;
 import com.esc.wmg.repository.UserRepository;
-
-<<<<<<< HEAD
-=======
 import jakarta.servlet.http.HttpSession;
->>>>>>> test
 
 @Controller
 public class UserController {
@@ -20,7 +16,6 @@ public class UserController {
     @Autowired
     UserRepository repository;
     
-<<<<<<< HEAD
 
     // 회원정보 수정기능
     @PostMapping("/UserUpdate")
@@ -31,17 +26,12 @@ public class UserController {
     
 
     // 회원가입 기능
-=======
-    //회원가입
->>>>>>> test
     @PostMapping("/userInsert")
     public String userInsert(UserEntity entity) {
         System.out.println(entity.toString());
         repository.save(entity);
         return "redirect:/";
     }
-<<<<<<< HEAD
-=======
     //로그인
     @PostMapping("/userSelect")
     public String userSelect(User user, HttpSession session) {
@@ -50,7 +40,6 @@ public class UserController {
         System.out.println(userEntity.toString());
         return "loginSuccess";
     }
->>>>>>> test
 
     @GetMapping("/")
     public String loginForm() {
