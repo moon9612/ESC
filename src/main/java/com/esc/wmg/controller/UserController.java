ed1025008e812be2f4aff6d0c42f97403a484ae3
jelly <sonho0515@naver.com>
2025-04-10 11:51:36 +0900

    로그인

--- src/main/java/com/esc/wmg/controller/UserController.java
+++ src/main/java/com/esc/wmg/controller/UserController.java
@@ -6,7 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.esc.wmg.entity.UserEntity;
+import com.esc.wmg.model.User;
 import com.esc.wmg.repository.UserRepository;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class UserController {
@@ -14,13 +17,21 @@ public class UserController {
     @Autowired
     UserRepository repository;
     
+    //회원가입
     @PostMapping("/userInsert")
     public String userInsert(UserEntity entity) {
         System.out.println(entity.toString());
         repository.save(entity);
         return "redirect:/";
     }
-
+    //로그인
+    @PostMapping("/userSelect")
+    public String userSelect(User user, HttpSession session) {
+        UserEntity userEntity = repository.findByEmailAndPw(user.getEmail(), user.getPw());
+        session.setAttribute("loginUser", userEntity);
+        System.out.println(userEntity.toString());
+        return "loginSuccess";
+    }
 
     @GetMapping("/")
     public String loginForm() {
