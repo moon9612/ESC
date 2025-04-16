c32e81b784bd84dbe1d5e87e34581020faad5e15
yj <dfyd15937@gmail.com>
2025-04-16 14:40:46 +0900

    a

--- src/main/java/com/esc/wmg/controller/UserController.java
+++ src/main/java/com/esc/wmg/controller/UserController.java
@@ -2,11 +2,14 @@ package com.esc.wmg.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.esc.wmg.entity.UserEntity;
 import com.esc.wmg.repository.UserRepository;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class UserController {
@@ -14,20 +17,34 @@ public class UserController {
     @Autowired
     UserRepository repository;
 
-    // 회원가입 기능
-    @PostMapping("/userInsert")
-    public String userInsert(UserEntity entity) {
-<<<<<<< HEAD
-        System.out.println("폼 데이터: " + toString());
+    // 로그인 기능
+    @PostMapping("/userSelect")
+    public String userSelect(UserEntity entity, HttpSession session, Model model) {
+        UserEntity loginUser = repository.findByEmailAndPw(entity.getEmail(), entity.getPw());
     
-        repository.save(entity); // 기존 테이블(tbl_user)에 insert됨
-=======
-        System.out.println(entity.toString());
-        repository.save(entity);
->>>>>>> test4
+        if (loginUser != null) {
+            session.setAttribute("loginUser", loginUser);
+            return "redirect:/"; 
+        } else {
+            return "login"; 
+        }
+    }
+
+    // 로그아웃 기능
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
         return "redirect:/";
     }
     
+
+    // 회원가입 기능
+    @PostMapping("/userInsert")
+    public String userInsert(UserEntity entity) {
+        System.out.println(entity.toString());
+        repository.save(entity);
+        return "redirect:/";
+    }
 
     // 로그인 페이지로 이동
     @GetMapping("/goLogin")
