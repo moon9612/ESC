4571af75326ba345710dcc6b6fb7d221bdf871ad
jelly <sonho0515@naver.com>
2025-04-29 16:54:10 +0900

    코드 통합 댓글 닉네임 출력

--- src/main/java/com/esc/wmg/entity/CommentEntity.java
+++ src/main/java/com/esc/wmg/entity/CommentEntity.java
@@ -5,9 +5,12 @@ import java.time.LocalDateTime;
 import io.micrometer.common.lang.NonNull;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,7 +22,6 @@ import lombok.RequiredArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@RequiredArgsConstructor
 public class CommentEntity {
 
     @Id
@@ -46,4 +48,7 @@ public class CommentEntity {
     @Column(name = "parent_idx")
     private Long parentIdx;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "email", referencedColumnName = "email", insertable = false, updatable = false)
+    private UserEntity user;
 }
