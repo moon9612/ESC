ec22dfb348196c15ddd3b3ac9481f39652c90147
jelly <sonho0515@naver.com>
2025-04-29 14:25:21 +0900

    코드 통합

---
+++ src/main/java/com/esc/wmg/repository/NewsRepository.java
@@ -0,0 +1,9 @@
+package com.esc.wmg.repository;
+
+import com.esc.wmg.entity.NewsEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NewsRepository extends JpaRepository<NewsEntity, Long> {
+}
