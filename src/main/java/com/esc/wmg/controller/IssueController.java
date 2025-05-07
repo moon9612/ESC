2ac97300320542c6d7211b39d42eb7032badfb13
jelly <sonho0515@naver.com>
2025-05-07 14:20:59 +0900

    이슈 페이지

--- src/main/java/com/esc/wmg/controller/IssueController.java
+++ src/main/java/com/esc/wmg/controller/IssueController.java
@@ -9,6 +9,9 @@ public class IssueController {
     // Issue 페이지 이동
     @GetMapping("/issues")
     public String issuesPage() {
+        // issue 키워드는 main에서 로드해서 session에 저장된 상태
+        // issue 키워드 관련 뉴스 로드
+        
         return "issues"; // issue.html로 이동
     }
 }
