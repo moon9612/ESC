928a6091c33ff578b191bd67435b966425aff2cc
jelly <sonho0515@naver.com>
2025-05-07 14:23:02 +0900

    s

--- src/main/java/com/esc/wmg/entity/NewsClusterEntity.java
+++ src/main/java/com/esc/wmg/entity/NewsClusterEntity.java
@@ -1,22 +1,22 @@
-// package com.esc.wmg.entity;
+package com.esc.wmg.entity;
 
-// import java.time.LocalDateTime;
+import java.time.LocalDateTime;
 
-// import jakarta.persistence.Column;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.Table;
-// import lombok.AllArgsConstructor;
-// import lombok.Data;
-// import lombok.NoArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-// @Entity
-// @Table(name = "tbl_news_cluster")
-// @Data
-// @AllArgsConstructor
-// @NoArgsConstructor
-// public class NewsClusterEntity {
-//     @Column(name = "date", updatable = false)
-//     private LocalDateTime date;
+@Entity
+@Table(name = "tbl_news_cluster")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NewsClusterEntity {
+    @Column(name = "date", updatable = false)
+    private LocalDateTime date;
 
 
-// }
+}
