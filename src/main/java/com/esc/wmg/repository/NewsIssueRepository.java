93182efb368c7357303d874e2dbc4f5f3bc8ab2f
yj <dfyd15937@gmail.com>
2025-05-07 16:45:15 +0900

    이슈키워드

---
+++ src/main/java/com/esc/wmg/repository/NewsIssueRepository.java
@@ -0,0 +1,18 @@
+package com.esc.wmg.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import com.esc.wmg.entity.NewsIssueEntity;
+
+@Repository
+public interface NewsIssueRepository extends JpaRepository<NewsIssueEntity, Long> {
+
+    // NewsRepository.java (최근 10개 데이터 조회)
+    @Query("SELECT n FROM NewsIssueEntity n ORDER BY n.date DESC, n.rnk ASC")
+    List<NewsIssueEntity> findTop10ByOrderByDateDescRnkAsc();
+
+}
