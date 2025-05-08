2e901af4c26712b8ee1cd482705c5cd3929b1f7e
jelly <sonho0515@naver.com>
2025-05-08 11:05:13 +0900

    최근 이슈 10개 데이터만 조회하도록 쿼리문 주석처리

--- src/main/java/com/esc/wmg/repository/NewsIssueRepository.java
+++ src/main/java/com/esc/wmg/repository/NewsIssueRepository.java
@@ -12,7 +12,7 @@ import com.esc.wmg.entity.NewsIssueEntity;
 public interface NewsIssueRepository extends JpaRepository<NewsIssueEntity, Long> {
 
     // NewsRepository.java (최근 10개 데이터 조회)
-    @Query("SELECT n FROM NewsIssueEntity n ORDER BY n.date DESC, n.rnk ASC")
+    // @Query("SELECT n FROM NewsIssueEntity n ORDER BY n.date DESC, n.rnk ASC")
     List<NewsIssueEntity> findTop10ByOrderByDateDescRnkAsc();
 
 }
