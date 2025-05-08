953484a2c2669a224017d267fa46e17ef67cc684
yj <dfyd15937@gmail.com>
2025-05-08 12:21:19 +0900

    a

--- src/main/java/com/esc/wmg/controller/IssueController.java
+++ src/main/java/com/esc/wmg/controller/IssueController.java
@@ -34,7 +34,7 @@ public class IssueController {
         System.out.println("newsCluster: " + newsCluster.size()); // 디버깅용 출력
         model.addAttribute("newsCluster", newsCluster); // 뉴스 클러스터 리스트를 모델에 추가
         // issue 키워드 관련 뉴스 로드
-        List<IssueStatisticsEntity> issueStatistics = IssueStatisticsRepository.findAll();
+        List<IssueStatisticsEntity> issueStatistics = issueStatisticsRepository.findAll();
         System.out.println("newsCluster: " + newsCluster.size()); // 디버깅용 출력
         model.addAttribute("newsCluster", newsCluster); // 뉴스 클러스터 리스트를 모델에 추가
 
