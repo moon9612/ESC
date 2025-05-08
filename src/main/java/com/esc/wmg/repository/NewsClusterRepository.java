e550f11dc33c1f450f4e067c7ccaec2d55b232a7
jelly <sonho0515@naver.com>
2025-05-08 12:37:46 +0900

    필요없는 부분 삭제

--- src/main/java/com/esc/wmg/repository/NewsClusterRepository.java
+++ src/main/java/com/esc/wmg/repository/NewsClusterRepository.java
@@ -3,7 +3,6 @@ package com.esc.wmg.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.esc.wmg.entity.NewsClusterEntity;
