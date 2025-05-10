74e41eb64d1762bb58b8e409a84b8f5b684d760b
jelly <sonho0515@naver.com>
2025-05-10 16:22:30 +0900

    뉴스 클러스터 오늘 날짜만 가져오면 similarity 내림차순

--- src/main/java/com/esc/wmg/entity/TblNewsClusterId.java
+++ src/main/java/com/esc/wmg/entity/TblNewsClusterId.java
@@ -5,12 +5,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class TblNewsClusterId implements Serializable {
-    private String date; // String으로 변경
+    private LocalDateTime date; // String으로 변경
     private Integer mdlIndex;
     private Integer seq;
 }
--- src/main/java/com/esc/wmg/repository/NewsClusterRepository.java
+++ src/main/java/com/esc/wmg/repository/NewsClusterRepository.java
@@ -1,5 +1,6 @@
 package com.esc.wmg.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,8 @@ import com.esc.wmg.entity.TblNewsClusterId;
 @Repository
 public interface NewsClusterRepository extends JpaRepository<NewsClusterEntity, TblNewsClusterId> {
     List<NewsClusterEntity> findAllByOrderBySimilarityDesc();
+
+    // Add method to find news clusters by date and order by similarity descending
+    // List<NewsClusterEntity> findByDateOrderBySimilarityDesc(LocalDate date);
+    List<NewsClusterEntity> findByDateBetweenOrderBySimilarityDesc(LocalDateTime start, LocalDateTime end);
 }
